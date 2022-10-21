e8f07754116dc9bca589c5a72f63dbb1faf58d99
Stephen <stephennnabuike@gmail.com>
2022-10-21 23:18:21 +0100

    Add scope to User model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments
-  
-  scope :three_recent_posts, ->  { Post.joins(:user).where(user_id: 1).limit(3).order(created_at: :desc) }
+  has_many :comments 
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   
-  scope :three_recent_posts_with_posts, -> {}    
+  scope :three_recent_posts, -> { Post.where(user_id: 2).limit(3).order(created_at: :desc) }   
 end
