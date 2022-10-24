00c2048841fc938ccc2a1242b21d552e569cc73c
Stephen <stephennnabuike@gmail.com>
2022-10-24 18:49:54 +0100

    Fix linters error

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,3 @@ RSpec.describe User, type: :model do
     expect(subject).to_not be_valid
   end
 end
-
--- spec/requests/post_spec.rb
+++ spec/requests/post_spec.rb
@@ -2,26 +2,26 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET #index' do
-    before(:example) { get "/users/:user_id/posts" } 
-    it "is a success" do
+    before(:example) { get '/users/:user_id/posts' }
+    it 'is a success' do
       expect(response).to have_http_status(:ok)
-    end    
+    end
 
-    it "include post index template content" do
-      expect(response.body).to include("Here are list of posts for a given user")
+    it 'include post index template content' do
+      expect(response.body).to include('Here are list of posts for a given user')
     end
   end
 end
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET #id' do
-    before(:example) { get "/users/:user_id/posts/:id" } 
-    it "is a success" do
+    before(:example) { get '/users/:user_id/posts/:id' }
+    it 'is a success' do
       expect(response).to have_http_status(:ok)
-    end    
+    end
 
-    it "include show template content" do
-      expect(response.body).to include("Here are list of posts for a given user and a given post")
+    it 'include show template content' do
+      expect(response.body).to include('Here are list of posts for a given user and a given post')
     end
   end
 end
