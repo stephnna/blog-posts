b7a72eb8e541a5f9a6991176c3bc9759e5aa861a
Stephen <stephennnabuike@gmail.com>
2022-10-24 18:43:24 +0100

    test controller files

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -2,13 +2,26 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET #index' do
-    before(:example) { get "http://localhost:3000/users" }  # get(:index)
+    before(:example) { get "/users" }  # get(:index)
     it "is a success" do
       expect(response).to have_http_status(:ok)
-    end
+    end    
 
-    it "renders 'index' template" do
-      expect(response).to render_template('index')
+    it "include template content" do
+      expect(response.body).to include("Here is a list for users")
+    end
+  end
+end
+
+RSpec.describe 'Users', type: :request do
+  describe 'GET #id' do
+    before(:example) { get "/users/:id" } 
+    it "is a success" do
+      expect(response).to have_http_status(:ok)
+    end    
+
+    it "include template content" do
+      expect(response.body).to include("Each User details goes here!")
     end
   end
 end
