80ac67c02183f2ae107652c1b095a3ba1cec57e5
Stephen <stephennnabuike@gmail.com>
2022-11-10 16:28:07 +0100

    Drop, create and migrate the database

--- db/migrate/20221020124332_remove_column_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnFromUsers < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :name, :string
-  end
-end
--- db/migrate/20221110103741_add_devise_to_users.rb
+++ db/migrate/20221110103741_add_devise_to_users.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class AddDeviseToUsers < ActiveRecord::Migration[7.0]
   def self.up
     change_table :users do |t|
@@ -37,7 +35,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
       # t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
+    add_index :users, :email, unique: true
     add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
