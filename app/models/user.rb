66b87eec38df4e5b7a827d9894875008db5e690b
Stephen Ezea <stephennnabuike@gmail.com>
2022-12-07 15:20:37 +0100

    Revert "Add devise"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
