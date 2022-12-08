84d00331c3bf0035994308c7beafb704dccc40c5
Stephen <stephennnabuike@gmail.com>
2022-12-08 10:29:32 +0100

    Fix authorization error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  before_action :set_global_variables
+  
   before_action :authenticate_user!
   before_action :update_allowed_parameters, if: :devise_controller?
 
-  protected
-
-  # vital for my navigation bar
-  def set_global_variables
-    @user = current_user
-  end
+  protected 
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :photo, :bio, :email, :password) }
     devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name, :email, :password, :current_password)
+      u.permit(:name, :photo, :bio, :email, :password, :current_password)
     end
   end
 end
